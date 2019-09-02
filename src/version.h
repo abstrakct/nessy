9eb5a1d8367264e15dabfac946bab0a6d55b64e6
Rolf K <abstractstar@gmail.com>
2019-09-02 09:12:34 +0200

    Silence some 'misleading indentation' warnings in olcPixelGameEngine

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
-#define VERSION_MAJOR 6c602c1-dirty
+#define VERSION_MAJOR 14f6a8a-dirty
 #define VERSION_MINOR 
 #define VERSION_REVISION 
 #define VERSION_HASH ""
-#define VERSION_STRING "6c602c1-dirty..."
+#define VERSION_STRING "14f6a8a-dirty..."
 #endif

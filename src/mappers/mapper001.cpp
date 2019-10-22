7b4a51bc47d9d3ad278d50f0becd8e338c01ef2a
Rolf K <abstractstar@gmail.com>
2019-10-22 18:17:22 +0200

    Mapper 1 improvements.

--- src/mappers/mapper001.cpp
+++ src/mappers/mapper001.cpp
@@ -1,49 +1,147 @@
+#include <string>
 #include "mapper001.h"
 
 
 Mapper001::Mapper001(uint8_t p, uint8_t c) : Mapper(p, c)
 {
+    writes = tmpreg = reg[1] = reg[2] = reg[3] = 0;
+    reg[0] = 0x0C;
+    lastBankOffset = (p - 1) * 0x4000;
+    vram.resize(0x2000);
 }
 
 Mapper001::~Mapper001()
 {
 }
 
+bool Mapper001::getMirrorType(int &data)
+{
+    data = reg[0] & 0x03;
+    return true;
+}
+
 bool Mapper001::cpuRead(uint16_t addr, uint32_t &mapped_addr)
 {
-    if (addr >= 0x8000) {
-        //mapped_addr = addr & (prgBanks > 1 ? 0x7FFF : 0x3FFF);
-        return true;
+    uint8_t mode = reg[0] & 0b1100;
+    if (mode == 0xC) {
+        // Highest bank fixed, swapable bank at 0x8000
+        if (addr >= 0x8000 && addr < 0xC000) {
+            mapped_addr = (addr & 0x7FFF) + ((reg[3] & 0xF) * 0x4000);
+            //printf("addr: %04X\tmapped_addr: %05X\n", addr, mapped_addr);
+            return true;
+        } else if (addr >= 0xC000) {
+            mapped_addr = (addr - 0xC000) + lastBankOffset;
+            //printf("addr: %04X\tmapped_addr: %05X\n", addr, mapped_addr);
+            return true;
+        }
+    } else if (mode == 0x8) {
+        // Lowest bank fixed, swapable bank at 0xC000
+        if (addr >= 0x8000 && addr < 0xC000) {
+            mapped_addr =  addr - 0x8000;
+            return true;
+        } else if (addr >= 0xC000) {
+            mapped_addr = (addr & 0x3FFF) + ((reg[3] & 0xF) * 0x4000);
+            return true;
+        }
+    } else if (mode == 0x0 || mode == 0x1) {
+        // Swapable bank at 0x8000, size 0x8000
+        if (addr >= 0x8000) {
+            mapped_addr = (addr - 0x8000) + (((reg[3] & 0xF) >> 1) * 0x8000);
+            //printf("addr = %04X\tmapped_addr = %05X\n", addr, mapped_addr);
+            return true;
+        }
+    } else {
+        printf("[Mapper001] WARNING unimplemented mode %X\n", mode);
+        return false;
     }
     return false;
 }
 
 bool Mapper001::cpuWrite(uint16_t addr, uint32_t &mapped_addr)
 {
-    if (addr >= 0x8000) {
-        //mapped_addr = addr & (prgBanks > 1 ? 0x7FFF : 0x3FFF);
+    if (addr >= 0x6000 && addr < 0x8000) {
+        mapped_addr = addr - 0x6000;
         return true;
     }
+    return false;
+}
+
+bool Mapper001::cpuWriteData(uint16_t addr, uint8_t data)
+{
+    if (addr & 0x8000) {
+        if (data & 0x80) {
+            // Reset
+            writes = 0;
+            tmpreg = 0;
+            reg[0] |= 0x0C;
+            // apply()
+        } else {
+            tmpreg = ((data & 0x01) << 4) | (tmpreg >> 1);
+            if (++writes == 5) {
+                //printf("[Mapper001] reg[%d] set to %02X (%02X)\n", (addr >> 13) & 0b11, tmpreg, (tmpreg & 0xF) >> 1);
+                reg[(addr >> 13) & 0b11] = tmpreg;
+                writes = 0;
+                tmpreg = 0;
+                // apply()
+            }
+        }
+
+        return true;
+        }
+
     return false;
 }
 
 bool Mapper001::ppuRead(uint16_t addr, uint32_t &mapped_addr)
 {
-    if (addr <= 0x1FFF) {
-        mapped_addr = addr;
-        return true;
-    }
-    return false;
-}
-
-bool Mapper001::ppuWrite(uint16_t addr, uint32_t &mapped_addr)
-{
-    if (addr <= 0x1FFF) {
-        if (chrBanks == 0) {
-            mapped_addr = addr;
+    uint8_t fourKMode = (reg[0] & 0x10) >> 4;
+    if (chrBanks > 0) {
+        if (addr < 0x2000) {
+            if (fourKMode) {
+                if (addr < 0x1000)
+                    mapped_addr = (addr & 0x1FFF) + (reg[1] * 0x1000);
+                else if (addr < 0x2000)
+                    mapped_addr = (addr & 0x0FFF) + (reg[2] * 0x1000);
+            } else {
+                mapped_addr = (addr & 0x1FFF) + ((reg[1] >> 1) * 0x2000);
+            }
             return true;
         }
     }
     return false;
 }
 
+bool Mapper001::ppuReadData(uint16_t addr, uint8_t &data)
+{
+    if (addr < 0x2000) {
+        data = vram[addr];
+        return true;
+    }
+
+    return false;
+}
+
+bool Mapper001::ppuWrite(uint16_t addr, uint32_t &mapped_addr)
+{
+    //printf("Mapper001::ppuWrite %04X\n", addr);
+    //if (chrBanks > 0) {
+    //    if (addr < 0x2000) {
+    //        mapped_addr = addr;
+    //        return true;
+    //    }
+    //} else {
+    //    if (addr < 0x2000) {
+
+    //}
+    return false;
+}
+
+bool Mapper001::ppuWriteData(uint16_t addr, uint8_t data)
+{
+    if (addr < 0x2000) {
+        vram[addr] = data;
+        return true;
+    }
+
+    return false;
+}
--- src/mappers/mapper001.h
+++ src/mappers/mapper001.h
@@ -1,19 +1,29 @@
 #pragma once
+
+#include <vector>
 #include "../mapper.h"
 
 class Mapper001 : public Mapper {
+    private:
+        uint8_t writes;
+        uint8_t tmpreg;
+        uint8_t reg[4];
+        uint32_t lastBankOffset;
+        std::vector<uint8_t> vram;
+
     public:
         Mapper001(uint8_t p, uint8_t c);
         ~Mapper001();
 
         bool cpuRead(uint16_t addr, uint32_t &mapped_addr) override;
         bool cpuWrite(uint16_t addr, uint32_t &mapped_addr) override;
-        bool cpuWriteData(uint16_t addr, uint8_t data) override { return false; };
+        bool cpuWriteData(uint16_t addr, uint8_t data) override;
         bool ppuRead(uint16_t addr, uint32_t &mapped_addr) override;
-        bool ppuReadData(uint16_t addr, uint8_t &data) override { return false; };
+        bool ppuReadData(uint16_t addr, uint8_t &data) override;
         bool ppuWrite(uint16_t addr, uint32_t &mapped_addr) override;
-        bool ppuWriteData(uint16_t addr, uint8_t data) override { return false; };
+        bool ppuWriteData(uint16_t addr, uint8_t data) override;
+        bool getMirrorType(int &data) override;
 
-        int implementationStatus() override { return MI_NOT_USABLE; };
+        int implementationStatus() override { return MI_DEVELOPMENT; };
 };
 

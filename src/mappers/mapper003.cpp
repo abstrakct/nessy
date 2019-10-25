b49f8e82a01b996ac809cf34b117b8c20a36cb36
Rolf K <abstractstar@gmail.com>
2019-10-25 14:04:55 +0200

    Added mapper 003 (doesn't work yet though)

---
+++ src/mappers/mapper003.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include "mapper003.h"
+
+
+
+Mapper003::Mapper003(uint8_t p, uint8_t c) : Mapper(p, c)
+{
+    // 8K VRAM (CHR RAM)
+    vram.resize(0x2000);
+}
+
+Mapper003::~Mapper003()
+{
+}
+
+std::vector<std::string> Mapper003::getInfoStrings()
+{
+    return infoString;
+}
+
+void Mapper003::reset()
+{
+    prgROM->setBank(0x8000, 0);
+    prgROM->setBank(0xC000, 1);
+
+    chrROM->setBank(0x0000, 0);
+    chrROM->setBank(0x1000, 1);
+}
+
+void Mapper003::apply()
+{
+    chrROM->setBank(0x0000, (chrBank * 2) + 0);
+    chrROM->setBank(0x1000, (chrBank * 2) + 1);
+}
+
+bool Mapper003::cpuRead(uint16_t addr, uint32_t &mapped_addr, bool &prgram)
+{
+    prgram = false;
+    if (addr >= 0x8000) {
+        return true;
+    }
+    return false;
+}
+
+bool Mapper003::cpuWrite(uint16_t addr, uint32_t &mapped_addr)
+{
+    return false;
+}
+
+bool Mapper003::cpuWriteData(uint16_t addr, uint8_t data)
+{
+    if (addr >= 0x8000) {
+        if (chrBanks > 4) {
+            chrBank = data;
+        } else {
+            chrBank = (data & 0x3);
+        }
+
+        apply();
+        printf("[Mapper003] chrBank set to %d (%02X)\n", chrBank, chrBank);
+
+        return true;
+    }
+
+    return false;
+}
+
+
+bool Mapper003::ppuRead(uint16_t addr, uint32_t &mapped_addr)
+{
+    return false;
+}
+
+bool Mapper003::ppuReadData(uint16_t addr, uint8_t &data)
+{
+    return false;
+}
+
+bool Mapper003::ppuWrite(uint16_t addr, uint32_t &mapped_addr)
+{
+    return false;
+}
+
+bool Mapper003::ppuWriteData(uint16_t addr, uint8_t data)
+{
+    if (addr < 0x2000)
+        vram[addr] = data;
+    return false;
+}
+
---
+++ src/mappers/mapper003.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+#include "../mapper.h"
+
+class Mapper003 : public Mapper {
+    public:
+        Mapper003(uint8_t p, uint8_t c);
+        ~Mapper003();
+
+        bool cpuRead(uint16_t addr, uint32_t &mapped_addr, bool &prgram) override;
+        bool cpuWrite(uint16_t addr, uint32_t &mapped_addr) override;
+        bool cpuWriteData(uint16_t addr, uint8_t data) override;
+        bool ppuRead(uint16_t addr, uint32_t &mapped_addr) override;
+        bool ppuReadData(uint16_t addr, uint8_t &data) override;
+        bool ppuWrite(uint16_t addr, uint32_t &mapped_addr) override;
+        bool ppuWriteData(uint16_t addr, uint8_t data) override;
+        bool getMirrorType(int &data) override { return false; };
+        void reset() override;
+
+        int implementationStatus() override { return MI_WORKING; };
+        std::vector<std::string> getInfoStrings() override;
+
+        void apply();
+    private:
+        uint8_t chrBank  = 0;
+        std::vector<uint8_t> vram;
+        std::vector<std::string> infoString = { };
+};
+
+

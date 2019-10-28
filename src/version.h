1876d72394b2d276e8b6a6b95d0f6c8a2c780279
Rolf K <abstractstar@gmail.com>
2019-10-28 10:47:42 +0100

    version bump thing

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_REVISION 37
-#define VERSION_HASH "g84a0883-dirty"
-#define VERSION_STRING "0.1.37.g84a0883-dirty"
+#define VERSION_REVISION 38
+#define VERSION_HASH "g412ac7b-dirty"
+#define VERSION_STRING "0.1.38.g412ac7b-dirty"
 #endif

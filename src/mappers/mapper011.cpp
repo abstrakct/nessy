dfde20b6816155fa3e92980358ae790f6f4d97a5
Rolf K <abstractstar@gmail.com>
2020-10-22 11:25:18 +0200

    Update mapper 011

--- src/mappers/mapper011.cpp
+++ src/mappers/mapper011.cpp
@@ -38,22 +38,24 @@ std::vector<std::string> Mapper011::getInfoStrings()
 
 void Mapper011::reset()
 {
-    prgROM->setBank(0x8000, 0);
-    prgROM->setBank(0xC000, 1);
+    prgROM->setMappable(2);
+    prgROM->setBank(0x8000, 0, 0);
+    prgROM->setBank(0xC000, 1, 1);
 
-    chrROM->setBank(0x0000, 0);
-    chrROM->setBank(0x1000, 1);
+    chrROM->setMappable(2);
+    chrROM->setBank(0x0000, 0, 0);
+    chrROM->setBank(0x1000, 1, 1);
 
     updateInfo = true;
 }
 
 void Mapper011::apply()
 {
-    prgROM->setBank(0x8000, (selectedPrgBank * 2) + 0);
-    prgROM->setBank(0xC000, (selectedPrgBank * 2) + 1);
+    prgROM->setBank(0x8000, (selectedPrgBank * 2) + 0, 0);
+    prgROM->setBank(0xC000, (selectedPrgBank * 2) + 1, 1);
 
-    chrROM->setBank(0x0000, (selectedChrBank * 2) + 0);
-    chrROM->setBank(0x1000, (selectedChrBank * 2) + 1);
+    chrROM->setBank(0x0000, (selectedChrBank * 2) + 0, 0);
+    chrROM->setBank(0x1000, (selectedChrBank * 2) + 1, 1);
 
     updateInfo = true;
 }

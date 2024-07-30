db4201b857e91171a89389a8dc4533ceb2e2cda1
Rolf K <abstractstar@gmail.com>
2024-07-30 10:27:23 +0200

    Version bump

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_REVISION 93
-#define VERSION_HASH "g41d2a97-dirty"
-#define VERSION_STRING "0.1.93.g41d2a97-dirty"
+#define VERSION_REVISION 94
+#define VERSION_HASH "g6ac03d2-dirty"
+#define VERSION_STRING "0.1.94.g6ac03d2-dirty"
 #endif
